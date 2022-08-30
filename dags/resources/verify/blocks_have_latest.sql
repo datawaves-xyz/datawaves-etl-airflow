48ec0d2e2bfffa2048986077307a40da93b3a46f
onesuper <onesuperclark@gmail.com>
2022-08-30 15:48:41 +0800

    Fix verify sql

--- dags/resources/verify/blocks_have_latest.sql
+++ dags/resources/verify/blocks_have_latest.sql
@@ -1,7 +1,7 @@
 select if(
 (
   select count(1) as cnt
-  from `{{ params.schema_name }}.blocks`
-  where date(timestamp) = '{{ ds }}'
+  from {{ params.schema_name }}.blocks
+  where dt = '{{ ds }}'
 ) > 0, 1,
 cast((select 'There are no latest blocks') as integer))
